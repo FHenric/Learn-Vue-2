0f0b49a2916a15055e569ab5eb312a85c3770752
FHenric <ffilipehhenrique@gmail.com>
2022-04-05 18:56:27 -0300

    last commit

---
+++ hello-vue/src/components/Counter.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="box is-info">
+        <h2>The count is: {{count}}</h2>
+
+        <p>
+            <button v-on:click="count += 1">Increment</button>
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return { 
+            count: 0
+        }
+    },
+}
+</script>
+
+<style >
+    .box{
+        background: #e3e3e3;
+        padding: 10px;
+        border: 1px solid #c5c5c5;
+        margin-bottom: 5px;
+    }
+</style>
